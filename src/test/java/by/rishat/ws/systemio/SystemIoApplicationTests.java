176f751b4963bb1eead086f690fed84aac65d37f
Ришат <rrmukhamedshin@htc-cs.ru>
2026-03-20 12:53:09 +0300

    Init commit

---
+++ src/test/java/by/rishat/ws/systemio/SystemIoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.rishat.ws.systemio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
