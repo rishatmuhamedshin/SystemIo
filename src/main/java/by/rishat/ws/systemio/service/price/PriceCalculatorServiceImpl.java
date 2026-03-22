68f3e39deaa3761da6e6fcfb6e5afcdbc01ac532
Ришат <rrmukhamedshin@htc-cs.ru>
2026-03-22 23:31:39 +0300

    little log info

--- src/main/java/by/rishat/ws/systemio/service/price/PriceCalculatorServiceImpl.java
+++ src/main/java/by/rishat/ws/systemio/service/price/PriceCalculatorServiceImpl.java
@@ -7,6 +7,7 @@ import by.rishat.ws.systemio.repository.CouponRepository;
 import by.rishat.ws.systemio.repository.ProductRepository;
 import by.rishat.ws.systemio.service.PriceCalculatorService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -14,6 +15,7 @@ import java.math.RoundingMode;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PriceCalculatorServiceImpl implements PriceCalculatorService {
 
     private final ProductRepository productRepository;
@@ -27,7 +29,7 @@ public class PriceCalculatorServiceImpl implements PriceCalculatorService {
 
         BigDecimal price = product.getPrice();
 
-
+        log.debug("Начальная цена продукта '{}': {}", product.getName(), price);
         if (couponCode != null && !couponCode.isEmpty()) {
             var coupon = couponRepository.findByCode(couponCode)
                     .orElseThrow(() -> new InvalidCouponException( "Купона " + couponCode + " не существует"));
@@ -37,13 +39,18 @@ public class PriceCalculatorServiceImpl implements PriceCalculatorService {
                     discountPercent.divide(new BigDecimal("100"), 4, RoundingMode.HALF_UP)
             );
             price = price.multiply(discountMultiplier);
+            log.info("Купон применен: {}. Скидка: {}%. Цена после скидки: {}",
+                    couponCode, discountPercent, price);
         }
 
         Country country = Country.getByNumber(taxNumber);
         BigDecimal taxRate = BigDecimal.valueOf(country.getTaxRate());
+        log.debug("Страна: {} со ставкой налога: {}", country.name(), taxRate);
 
         BigDecimal taxMultiplier = BigDecimal.ONE.add(taxRate);
         BigDecimal finalPrice = price.multiply(taxMultiplier);
+
+        log.info("Окончательная цена {}", finalPrice);
 
         return finalPrice.setScale(2, RoundingMode.HALF_UP);
     }
